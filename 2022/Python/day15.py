0dc2a07dc3d58d9bf244fe4ae1b7c64b84967829
JLJVS <72031471+JLJVS@users.noreply.github.com>
2024-07-13 03:23:19 +0200

    Solution for AOC 2022 Day 15 part 2. This was brute force implementation and took roughly 15 min to run for part 2. Refactoring to improve speed might be needed.

--- 2022/Python/day15.py
+++ 2022/Python/day15.py
@@ -65,11 +65,10 @@ def part1(filepath, y= 2000000):
         
         sensor, beacon = coord
         x_sensor, y_sensor = sensor
-        x_beacon, y_beacon = beacon
-
+        
         # add the sensor and the beacon to our locations
-        locations[(x_sensor, y_sensor)] = "S"
-        locations[(x_beacon, y_beacon)] = "B"
+        locations[sensor] = "S"
+        locations[beacon] = "B"
 
         distance = manhattan_distance(sensor, beacon)
         
@@ -81,42 +80,85 @@ def part1(filepath, y= 2000000):
 
     possibles = list(possibles)
     possibles.sort()
-    #print(possibles)
-
-    #target = [i for i in locations.keys() if i[1]==y and locations[i]=="#"]
-
+    
     print("Part 1:")
     print(f"There are {len(possibles)} positions that cannot contain a beacon.")
-    
 
-def part2(filepath):
+def find_outline(coord: coord, distance) -> set[coord]:
+    '''
+    Finds the outline of the diamond around the scanner at the coordinate for a given distance. Returns a set of coords
+    '''
+
+    outline = set()
+    x, y = coord
+
+    for i in range(distance+1):
+        dx = distance-i
+        dy = i
+        outline.add((x+dx, y+dy))
+        outline.add((x+dx, y-dy))
+        outline.add((x-dx, y+dy))
+        outline.add((x-dx, y-dy))
+
+    return outline
+
+def get_neighbors(coord):
+    '''
+    Gets the neighbors for a specific coordinate
+    '''
+    x,y = coord
+    deltas = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    neighbors = []
+    for d in deltas:
+        dx, dy = d
+        neighbors.append((x+dx, y+dy))
+    return neighbors
+
+
+def part2(filepath, N_max=4000000):
     '''
     Finds the only possible location for a beacon not detected by our sensors and returns its tuning frequency=x*4000000+y
+
+    Usage example:
+    >>> part2(test15, 20)
+    Part 2:
+    The beacon can be found at (14, 11) with a resulting tuning frequency of 56000011.
     '''
     lines = read_input(filepath)
     coords = get_coords(lines)
-
-    # keep track of the sensor and beacons and use a set so we don't double count locations
-    not_possible = set()
-    N_max = 4000000
+    
     N_min = 0
 
+    outlines = set()
+
     for coord in coords:
-        
         sensor, beacon = coord
-        x_sensor, y_sensor = sensor
-        x_beacon, y_beacon = beacon
-
-        # add the sensor and the beacon to our locations
-        locations[(x_sensor, y_sensor)] = "S"
-        locations[(x_beacon, y_beacon)] = "B"
-
         distance = manhattan_distance(sensor, beacon)
-        
-        # only check positions with the correct y and all possible delta x < distance
-        for i in range(-distance, distance+1):
-            possible = (x_sensor+i, y)
-            if manhattan_distance(sensor, possible)<=distance and possible not in locations.keys():
-                    possibles.add(possible)
+        outline = find_outline(sensor, distance)
+        for pos in outline:
+            neighbors = set(get_neighbors(pos) )
+            outlines.update(neighbors)
 
+    # filter the outlines to be inside our desired range
+    outlines = [outline for outline in outlines if outline[0]>N_min and outline[0]<N_max and outline[1]>N_min and outline[1]<N_max]
+    
+    for target in outlines:
+        outside = True
+
+        for coord in coords:
+            sensor, beacon = coord
+            
+            distance = manhattan_distance(sensor, beacon)
+            target_distance = manhattan_distance(sensor, target)
+            if target_distance <= distance:
+                outside = False
+                break
+
+        if outside:
+            print("Part 2:")
+            print(f"The beacon can be found at {target} with a resulting tuning frequency of {target[0]*4000000  + target[1]}.")
+            break
+    
+    
 part1(filepath)
+part2(filepath)
--- 2022/Python/tempCodeRunnerFile.py
+++ 2022/Python/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-l, [r]
+
+    The beacon can be found at (14,11) with a resulting tuning frequency of 56000011.
