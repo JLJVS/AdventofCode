57d27662fcaf69a5fd7539385e31bf27fb268de3
JLJVS <72031471+JLJVS@users.noreply.github.com>
2024-07-18 23:33:08 +0200

    Current progress on AOC 2022 day 18 part 2, close to finished but not sure what's going wrong at the moment.

--- 2022/Python/day18.py
+++ 2022/Python/day18.py
@@ -72,48 +72,95 @@ def part1(filepath):
     print("Part 1:")
     print(f"The surface area of the lava droplet is {surface_area}.")
 
-def fill_cavities(coord: coord, grid: grid) -> set[coord]:
+def get_inner_surface_area(grid: grid) -> int:
     '''
-    Fils the cavities in the grid in a breadth first approach. We'll try to use a deque to improve the speed
+    Finds the inner cavities by creating a box around the cubes and fills the outside with a breadth first approach. It then iterates over the all entries in our filled box and finds the cavities.
+    
     '''
-    # just return an empty set if it's an "internal cube"
-    if get_free_sides(coord, grid)==0:
-        return set()
-    filled = set()
+    
+    
     cubes = set(cube for cube in grid.keys())
-    visited = set()
-    neighbors = get_neighbors(coord)
-    neighbors = set(neighbor for neighbor in neighbors if neighbor not in cubes)
-    for neighbor in neighbors:
-        visited.add(neighbor)
-        for _ in range(5):
-            new_neighbors = get_neighbors(neighbor)
-            old_size = len(visited)
-            possible = [neighbor]
-            for new_neighbor in new_neighbors:
-                not_in_cubes = new_neighbor not in cubes
-                not_in_visited = new_neighbor not in visited
-                if  not_in_cubes and not_in_visited:
-                    visited.add(new_neighbor)
-                    possible.append(new_neighbor)
+    
+    
+    # get the outer bounds 
+    min_x = min([cube[0] for cube in cubes])
+    max_x = max([cube[0] for cube in cubes])
+    min_x -= 1
+    max_x += 1
+    
+    min_y = min([cube[1] for cube in cubes])
+    max_y = max([cube[1] for cube in cubes])
+    min_y -= 1
+    max_y -= 1
+    
 
-            new_size = len(visited)
-            if old_size == new_size:
-                for pos in possible:
-                    filled.add(pos)
-                
-        possible = new_neighbors
-        #print(visited)
-        print(filled)
-    return filled
+    min_z = min([cube[2] for cube in cubes])
+    max_z = max([cube[2] for cube in cubes])
+    min_z -= 1
+    max_z += 1
+
+    # create the boolean conditions to determine if we're inside our bounds
+    x_allowed = lambda x: x>= min_x and x<=max_x
+    y_allowed = lambda y: y>= min_y and y<=max_y
+    z_allowed = lambda z: z>= min_z and z<=max_z
+
+    # start at the boundary and all the cubes we can reach are outside the cube
+    start_min = (min_x, min_y, min_z)
+    start_max = (max_x, max_y, max_z)
+    outside = set()
+    
+    outside_queue = deque() 
+    outside_queue.append(start_min)
+    outside_queue.append(start_max)
+    while outside_queue:
+        coord = outside_queue.popleft()
+        outside.add(tuple(coord))
+        neighbors = get_neighbors(coord)
+        for neighbor in neighbors:
+            x, y, z = neighbor
+            if not x_allowed(x) or not y_allowed(y) or not z_allowed(z):
+                continue
+            elif neighbor in cubes or neighbor in outside:
+                continue
+            else:
+                outside.add(tuple(neighbor))
+                outside_queue.append(neighbor)
+    outside = list(outside)
+    outside.sort()
+    inside = dict()
+    for x in range(min_x, max_x+1):
+        for y in range(min_y, max_y+1):
+            for z in range(min_z, max_z+1):
+                if (x, y, z) not in outside and (x, y, z) not in cubes:
+                    inside[(x,y,z)] = 1
+
+    inner_surface_area = 0
+    for coord in inside:
+        inner_surface_area += get_free_sides(coord, inside)
+    
+    return inner_surface_area
+
+def part2(filepath):
+    '''
+    Finds the surface area of the cubes with the inner surface areas removed.
+
+    Usage example:
+    >>> part2(test18)
+    Part 2:
+    The exterior surface area is 58.
+    '''
+
+    lines = read_input(filepath)
+    grid = convert_to_grid(lines)
+
+    total_surface_area = 0
+    for cube in grid:
+        total_surface_area += get_free_sides(cube, grid)    
+
+    inner_surface_area = get_inner_surface_area(grid)
+    print("Part 2:")
+    print(f"The exterior surface area is {total_surface_area-inner_surface_area}.")
+    
 
 part1(filepath)
-
-lines = read_input(test18)
-grid = convert_to_grid(lines)
-
-print(len(grid))
-to_fill =  set()
-for coord in [(2,2,4)]:
-    to_fill.update(fill_cavities(coord, grid))
-print(to_fill)
+part2(filepath)
