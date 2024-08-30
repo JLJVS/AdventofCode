ae1f9d6e47a9ff7f9d9325df8a34805c3dca27f0
JLJVS <72031471+JLJVS@users.noreply.github.com>
2024-08-30 22:30:52 +0200

    Solution for AOC 2023 Python day 16 part 1 and part2.

--- 2023/Python/day16.py
+++ 2023/Python/day16.py
@@ -27,52 +27,40 @@ def get_grid(lines: list[str]) -> Grid:
 
     return grid
 
-def split_vertical(coord: Coord, grid: Grid) -> List[Tuple[Coord, Heading]]:
+def split_vertical(coord: Coord) -> List[Tuple[Coord, Heading]]:
     '''
     when approaching a | from the left or the right
     '''
     return [(coord, (0, -1)), (coord, (0, 1))]
     
 
-def split_horizontal(coord: Coord, grid: Grid) -> List[Tuple[Coord, Heading]]:
+def split_horizontal(coord: Coord) -> List[Tuple[Coord, Heading]]:
     '''
     when approaching a - from below or above
     '''
     return [(coord, (-1, 0)), (coord, (1, 0))]
     
 
-def mirror_right(coord: Coord, heading: Heading, grid: Grid) -> Tuple[Coord, Heading]:
+def mirror_right(coord: Coord, heading: Heading) -> Tuple[Coord, Heading]:
     '''
-    mirror right /
+    mirror right / changes the heading to the new heading and returns a tuple of the coordinate and the heading
     '''
-    x, y = coord
-    new_heading = coord, heading
-    if heading == (1, 0):
-        new_heading = (0, -1)
-    elif heading == (-1, 0):
-        new_heading = (0, 1)
-    elif heading == (0, 1):
-        new_heading = (-1, 0)
-    else:
-        new_heading = (1, 0)
-    return [(coord, new_heading)]
+    headings = {(1, 0): (0, -1),
+                (-1, 0): (0, 1),
+                (0, 1): (-1, 0),
+                (0, -1): (1, 0)}
+    return [(coord, headings[heading])]
     
-def mirror_left(coord: Coord, heading: Heading, grid: Grid) -> Tuple[Coord, Heading]:
-    '''
-    mirror left \
-    '''
     
-    new_heading = heading
-    if heading == (1, 0):
-        new_heading = (0, 1)
-    elif heading == (-1, 0):
-        new_heading = (0, -1)
-    elif heading == (0, 1):
-        new_heading = (1, 0)
-    else:
-        new_heading = (-1, 0)
-
-    return [(coord, new_heading)]
+def mirror_left(coord: Coord, heading: Heading) -> Tuple[Coord, Heading]:
+    '''
+    mirror left \\ changes the heading to the new heading and returns a tuple of the coordinate and the heading
+    '''
+    headings = {(1, 0): (0, 1),
+                (-1, 0): (0, -1),
+                (0, 1): (1, 0),
+                (0, -1): (-1, 0)}
+    return [(coord, headings[heading])]
     
 
 def move(coord: Coord, heading: Heading, grid: Grid) -> List[Tuple[Coord, Heading]]:
@@ -84,47 +72,46 @@ def move(coord: Coord, heading: Heading, grid: Grid) -> List[Tuple[Coord, Headin
     new_x, new_y = x+dx, y+dy
     new_coord = (new_x, new_y)
     pos = grid.get(new_coord, (-1, -1))
-    if pos == (-1, -1):
-        return [(coord, heading)]
+    if pos == (-1, -1): # we moved off the grid
+        return []
     elif pos == ".":
         return [(new_coord, heading)]
     elif pos == "|":
-        if heading in [(1, 0), (-1, 0)]:
-            return split_vertical(new_coord, grid)
+        if heading in [(1, 0), (-1, 0)]: # approach from the left or right
+            return split_vertical(new_coord)
         else:
             return [(new_coord, heading)]
     elif pos == "-":
-        if heading in [(0, 1), (0, -1)]:
-            return split_horizontal(new_coord, grid)
+        if heading in [(0, 1), (0, -1)]: # approach from the top or bottom
+            return split_horizontal(new_coord)
         else:
             return [(new_coord, heading)]
     elif pos == "\\":
-        return mirror_left(new_coord, heading, grid)
+        return mirror_left(new_coord, heading)
     elif pos == "/":
-        return mirror_right(new_coord, heading, grid)
+        return mirror_right(new_coord, heading)
         
-def visit_all(start: Coord, grid: Grid, heading=(1,0), ):
+def visit_all(start: Coord, grid: Grid, heading=(1,0)):
     '''
     Moves 
     '''    
     visited = set()
     current = [(start, heading)]
-    for _ in range(800):
+    allowed = set(grid.keys())
+    for _ in range(1000):
         new_pos = []
         for pos in current:
             #print(i, pos)
             c, h = pos
-            # x, y = c
-            # dx, dy = h
             new_pos += move(c, h, grid)
-            # if new_pos[-1][0] != (x+dx, y+dy):
-            #     visited.add(((x+dx, y+dy), (0, 0)))
             visited.add(pos)
-        current = [i for i in list(set(new_pos)) if i[0] in grid.keys()]
+        current = [i for i in list(set(new_pos)) if i[0] in allowed] # check if they're in the grid
+        current = [i for i in current if i not in visited] # remove before seen position and heading combinations
     
-    sorted_visited = [i for i in set([i[0] for i in visited if i[0] in grid.keys()]) ] 
+    sorted_visited = [i for i in set([i[0] for i in visited if i[0] in allowed])] 
+    sorted_visited = list(set(sorted_visited))
     sorted_visited.sort()
-    print(sorted_visited)
+    
     return len(sorted_visited)
 
 def part1(filepath):
@@ -138,8 +125,39 @@ def part1(filepath):
     '''
     lines = read_input(filepath)
     grid = get_grid(lines)
-    energized = visit_all((0,0), grid, (1, 0))
+    energized = visit_all((-1,0), grid, (1, 0)) 
     print("Part 1:")
     print(f"{energized} tiles become energized.")
 
+def part2(filepath):
+    '''
+    Calculates how many tiles can be energized by starting from all starting positions at the edges.
+
+    Usage example:
+    >>> part2(test16)
+    Part 2:
+    51 tiles can be energized by starting at (3, -1).
+    '''
+    lines = read_input(filepath)
+    grid = get_grid(lines)
+    N = len(lines)
+    most_energized = 0
+    most_energized_start = (0,0)
+    top_start = [(i, -1, 0, 1) for i in range(N)]
+    bottom_start = [(i, N, 0, -1) for i in range(N)]
+    left_start = [(-1, i, 1, 0) for i in range(N)]
+    right_start = [(N, i, -1, 0) for i in range(N)]
+    all_starts = top_start + bottom_start + left_start + right_start
+    for start in all_starts:
+        x, y, dx, dy = start
+        energized = visit_all((x,y), grid, (dx, dy))
+        if energized > most_energized:
+            most_energized = energized
+            most_energized_start = (x, y)
+
+    print("Part 2:")
+    print(f"{most_energized} tiles can be energized by starting at {most_energized_start}.")
+    
+
 part1(filepath)
+part2(filepath)
