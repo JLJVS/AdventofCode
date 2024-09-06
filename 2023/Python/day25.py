59b2b100e3450dbd3a1f4b606cdbca61830bafd1
JLJVS <72031471+JLJVS@users.noreply.github.com>
2024-09-06 23:28:24 +0200

    Current progress AOC 2023 Python day 25 part1

--- 2023/Python/day25.py
+++ 2023/Python/day25.py
@@ -17,6 +17,7 @@ def create_network(lines: List[str]):
     Creates the network from the input
     '''
     network = dict()
+    edges = set()
     for line in lines:
         line = line.replace(":", " ")
         nodes = line.split()
@@ -26,14 +27,63 @@ def create_network(lines: List[str]):
         else:
             network[origin] = nodes[1:]
         for node in nodes[1:]:
+            edges.add(tuple([origin, node]))
             if node in network:
                 network[node] += [origin]
             else:
                 network[node] = [origin]
 
+    return network, edges
+def cut_connection(network, edge):
+    '''
+    Cuts the edge in the network
+    '''
+    
+    #print(edge)
+    origin, destination = edge[0], edge[1]
+    network[origin].remove(destination)
+    network[destination].remove(origin)
+    
     return network
 
 
+def find_networks(network, edge):
+    '''
+    Cuts the given edge out of the network and finds the newly formed networks
+    '''
+
+    new_networks = []
+    network = cut_connection(network, edge)
+    nodes = [i for i in network.keys()]
+    nodes_visited = set()
+    visited = set()
+    for node in nodes:
+        if node in visited:
+            #print("Already visited")
+            continue
+        current = [node]
+        while current:
+            #print(current, visited)
+            next = []
+            for n in current:
+                if n not in visited:
+                    visited.add(n)
+                    next += network[n]
+            current = next
+        if len(visited) == len(network):
+            return [len(visited)]
+        nodes_visited.update(visited)
+        new_networks.append(visited)
+    print(new_networks)
+    return [len(i) for i in new_networks]
+
+
+
+
 lines = read_input(test25)
-network = create_network(lines)
-print(network)
+network, edges = create_network(lines)
+#print(network)
+#for edge in edges:
+#    print(edge)
+#    print(find_networks(network, edge))
+print(find_networks(network, ("nvd", "jqt")))
